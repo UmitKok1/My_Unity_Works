e378bda2a40d7c1e0c125dad18a38b8e58556dd1
UmitKok21 <umit.kok21@gmail.com>
2021-05-17 00:25:55 +0300

    bike_game

---
+++ Biker/Assets/Scripts/ai.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class ai : MonoBehaviour
+{
+    //public Transform path;
+    private List<Transform> nodes;
+    public Transform target;
+    //Transform[] pathTransforms;
+    //public Transform start;
+    private void Start()
+    {
+        //pathTransforms = path.GetComponentsInChildren<Transform>();
+        //nodes = new List<Transform>();
+
+        //for (int i = 0; i < pathTransforms.Length; i++)
+        //{
+        //    if (pathTransforms[i] != path.transform)
+        //    {
+        //        nodes.Add(pathTransforms[i]);
+        //    }
+        //}
+
+        //GetComponent<NavMeshAgent>().destination = target.position;
+    }
+
+    private void FixedUpdate()
+    {
+        //for (int i = 1; i < nodes.Count; i++ )
+        //{
+        //    Debug.Log(pathTransforms[i].position);
+
+        //}
+        GetComponent<NavMeshAgent>().destination = target.position;
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag=="bullet")
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
---
+++ Biker/Assets/Scripts/bullet.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bullet : MonoBehaviour
+{
+    public float bulletSpeed;
+    Rigidbody rb;
+    void Start()
+    {
+        rb = FindObjectOfType<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb.velocity = transform.forward * bulletSpeed;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag=="bot")
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
